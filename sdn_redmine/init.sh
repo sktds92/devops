4a54641ab705340fa89f1a816e8be9d5bee8f865
seenpc15 <sktds92@seedn.co.kr>
2023-02-08 12:25:58 +0900

    first commit

---
+++ sdn_redmine/init.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#wysiwyg_editor download
+cd ../plugins && git clone https://github.com/taqueci/redmine_wysiwyg_editor.git
+
+#hidebar
+cd ../plugins && git clone https://gitlab.com/bdemirkir/sidebar_hide.git
+
+#puple themes download
+cd ../themes && git clone https://github.com/mrliptontea/PurpleMine2.git
